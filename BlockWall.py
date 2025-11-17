f39b2a02ece5a99a0b586feeb0d5a9ec2fe800c5
INFECTER <steven.hedgy@gmail.com>
2025-11-17 13:23:28 -0500

    Add files via upload

---
+++ BlockWall.py
@@ -0,0 +1,13 @@
+from BaseBlock import BaseBlock
+
+
+class BlockWall(BaseBlock):
+
+    def __init__(self,coor,blockId):
+        super().__init__(coor,blockId)
+
+    def stepOnApproval(self):
+        return False
+
+
+
