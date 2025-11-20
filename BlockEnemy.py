72df7b14cb7609b88bc661ab4107bc3239c147b2
QuantaSea <lovejuno2007@gmail.com>
2025-11-20 12:01:12 -0500

    Update BlockEnemy.py

--- BlockEnemy.py
+++ BlockEnemy.py
@@ -3,8 +3,9 @@ from BaseBlock import BaseBlock
 
 class BlockEnemy(BaseBlock):
 
-    def __init__(self,coor,blockId):
+    def __init__(self,coor,blockId,screen):
         super().__init__(coor,blockId)
+        self.screen = screen
 
     def stepOnApproval(self):
-        BattleLoop.battles()
+        BattleLoop.battles(self.screen)
