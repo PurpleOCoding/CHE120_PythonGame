73b76278c53fa6bd93b3b5ce0f5277d515f5a54f
QuantaSea <lovejuno2007@gmail.com>
2025-11-20 11:57:53 -0500

    Update BlockEnemy.py

--- BlockEnemy.py
+++ BlockEnemy.py
@@ -1,10 +1,10 @@
 from BaseBlock import BaseBlock
 
 
-class BlockWall(BaseBlock):
+class BlockEnemy(BaseBlock):
 
     def __init__(self,coor,blockId):
         super().__init__(coor,blockId)
 
     def stepOnApproval(self):
-        #cheese
+        BattleLoop.method()
