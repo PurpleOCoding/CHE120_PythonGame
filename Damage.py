4390d138d0fde048d381d7e23b312c61d15507f3
QuantaSea <lovejuno2007@gmail.com>
2025-11-29 00:19:33 -0500

    Update Damage.py

--- Damage.py
+++ Damage.py
@@ -4,40 +4,49 @@ Created on Mon Nov  6 13:30:30 2025
 
 @author: juno
 """
-
 import random
-#Whatever name is sent
-#from BattleLoop import Level
+
+#the following function assigns damage to either the player or the
+#professor based on whether the player answered the question correctly
 def Damage_Target(Answer, level):
+    '''
+    
+    '''
+    #if the player answered the question correctly
     if Answer:
+        #finds a random value of damage based on the student's Toool damage range
         dmg_roll = random.randrange(Student.low_range, Student.high_range)
-        #The updated health of the recieving character after the attack
+        
+        #assigns iq loss to the enemy, scaling with level
         Enemy.iq -= dmg_roll + (5 * level)
-        print(Enemy.iq)
     else:
+        #assigns a damage range to the professor based on level
         Enemy.low_range = 3 + (5 * level)
         Enemy.high_range = 7 + (5 * level)
         
+        #finds a random value of damage based on the professor's damage range
         dmg_roll = random.randrange(Enemy.low_range, Enemy.high_range)
 
         #attacks hit three-fourths of the time (25% of the time attack from enemy does not hit)
+        #if a random value from 0 to 3 is not 0
         if random.randrange(0,3) != 0:
-            #The updated health of the recieving character after the attack
+            #assigns player iq loss
             Student.iq -= dmg_roll
-            print(Student.iq)
 
     #if student has won
     if Enemy.iq <= 60:
-        print("E dead")
+        #return that the loop has ended (False) and that 
+        #the winner is the student (True), respectively
         return(False, True)
     #if enemy has won
     elif Student.iq <= 60:
-        print("icwnefin3iun4444554")
-        print("s dead")
+        #return that the loop has ended (False) and that 
+        #the winner is the professor (False), respectively
         return(False, False)
     #otherwise continue battle loop
     else:
-        print("NA")
+        #return that the loop continues (True) and that 
+        #a winner has not been determined (None), respectively
         return(True, None)
     
 def heal_student(heal):
@@ -45,14 +54,11 @@ def heal_student(heal):
     return Student.iq
     
 def heal_enemy(level):
-    print(level,"   levels")
-    print("afjlnegkmerneltkmglketmhr;ltkmh")
     if(level == 6):
         level = 1
     Enemy.iq = 100+(45)*(level-1)
     #else:
         #Enemy.iq = 100
-    print(Enemy.iq)
     heal = False
     
 def heals_enemy():
@@ -102,4 +108,5 @@ class Enemy(Tool):
         self.iq = iq
         self.low_range = low_range
         self.high_range = high_range
+
 
