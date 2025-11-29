733e9dd8adf9cdde5c9153386d320fe52537d8d9
QuantaSea <lovejuno2007@gmail.com>
2025-11-28 22:47:21 -0500

    Update GameGrid.py

--- GameGrid.py
+++ GameGrid.py
@@ -8,12 +8,14 @@ from Coor import Coor
 
 class GameGrid:
 
-
     def getNumberOfRows(self):
         return self.numRows
+
+    
     def getNumberOfColumns(self):
         return self.numCols
 
+    
     def createBlock(self, blockId, coor):
         block = None
         if blockId == "w":
@@ -29,26 +31,26 @@ class GameGrid:
 
         self.setBlock(block)
 
+    
     def getBlock(self,coor):
         return self.blocks[coor.get_x_coor()][coor.get_y_coor()]
 
+    
     def setBlock(self,block):
         coor = block.getCoordinate()
         self.blocks[coor.get_y_coor()][coor.get_x_coor()] = block
-        
+
+    
     def getBlockScaling(self,coor):
         return self.blocks[coor.get_x_coor()//40][coor.get_y_coor()//40]
+
     
     def setBlockScaling(self,block):
         coor = block.getCoordinate()
         self.blocks[coor.get_y_coor()//40][coor.get_x_coor()//40] = block
+
     
-
-
-
     def __init__(self,board):
-        
-
         
         self.numRows = len(board)
         self.numCols = len(board[0])
@@ -60,56 +62,8 @@ class GameGrid:
                 coor = Coor(x,y)
                 blockId = board[x][y]
                 self.createBlock(blockId,coor)
-                
-                
 
         for numRows in range(16):
             for numCols in range(16):
                 print(self.blocks[numCols][numRows].getId(),end="")
             print("\n")
-                #print(coor,end ="")
-            #print("\n")
-
-
-if __name__ == "__main__":
-    map_array_characters = [["w", "w", "w", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "w", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "w", "w", "w", "w", "w", "w", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "w", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        , ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]]
-
-    a = GameGrid(map_array_characters)
-    for i in range(16):
-        for j in range(16):
-            print(a.getBlock(Coor(j,i)).getId(),end="")
-        print("\n")
-
-
-
-
-
-    # a.setBlock(BlockWall(Coor(0,0),"w"))
-    # for i in range(16):
-    #     for j in range(16):
-    #         print(a.getBlock(Coor(j,i)).getId(),end="")
-    #     print("\n")
-
-    #print(a.getBlock(Coor(0,0)))
-
-
-
-
-
-
-
