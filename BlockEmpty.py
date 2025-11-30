421d17c43d6df889ce4c9698e6d0e8473ed0d120
INFECTER <steven.hedgy@gmail.com>
2025-11-30 10:20:35 -0500

    Commented BlockEmpty.py

--- BlockEmpty.py
+++ BlockEmpty.py
@@ -2,6 +2,9 @@ from BaseBlock import BaseBlock
 
 
 class BlockEmpty(BaseBlock):
-
+    """
+    This object is a subclass of BaseBlock representing a empty block
+    No additional functions have been given to this block
+    """
     def __init__(self,coor,blockId):
         super().__init__(coor,blockId)
