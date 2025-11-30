af1c7bc4494e6b3eee978bfdd8159446761d8243
INFECTER <steven.hedgy@gmail.com>
2025-11-30 10:22:23 -0500

    Commented BlockWall.py

--- BlockWall.py
+++ BlockWall.py
@@ -2,12 +2,17 @@ from BaseBlock import BaseBlock
 
 
 class BlockWall(BaseBlock):
-
+    """
+    This block is a subclass of BaseBlock representing a Wall
+    """
     def __init__(self,coor,blockId):
         super().__init__(coor,blockId)
 
     def stepOnApproval(self):
+        """
+        When called returns only False
+
+        () -> bool
+        """
         return False
-
-
 
