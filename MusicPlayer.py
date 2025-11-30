673424e580612aa00996992e58c3cf433c3a5369
INFECTER <steven.hedgy@gmail.com>
2025-11-30 10:23:16 -0500

    Commented MusicPlayer.py

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,38 +1,63 @@
-import pygame
-import os
+import pygame, sys, time
 
 
 def initilizeMusic():
+    """
+    Sets up pytgames music player so that music can be played
+
+    () -> None
+    """
     pygame.mixer.init()
 
 
 def playMusic(songNumber):
+    """
+    Plays music based on the associated number inputted
+
+    int -> None
+    """
 
     if(songNumber == 1):
-        pygame.mixer.music.load(str(os.getcwd()) + "\\Game Music\\1-06. Clutterfunk.mp3")
+        #Plays the song called Clutterfunk
+        pygame.mixer.music.load("1-06. Clutterfunk.mp3")
     elif(songNumber == 2):
-        pygame.mixer.music.load(str(os.getcwd()) + "\\Game Music\\4-09. Hexagon Force.mp3")
+        # Plays the song called Hexagon Force
+        pygame.mixer.music.load("4-09. Hexagon Force.mp3")
     elif(songNumber == 3):
-        pygame.mixer.music.load(str(os.getcwd()) + "\\Game Music\\4-08. Press Start.mp3")
+        # Plays the song called Press Start
+        pygame.mixer.music.load("4-08. Press Start.mp3")
     elif(songNumber == 4):
-        pygame.mixer.music.load(str(os.getcwd()) + "\\Game Music\\2-04. Stay Inside Me.mp3")
+        # Plays the song called Stay Inside Me
+        pygame.mixer.music.load("2-04. Stay Inside Me.mp3")
     elif(songNumber == 5):
-        pygame.mixer.music.load(str(os.getcwd()) + "\\Game Music\\4-07. Striker.mp3")
-
-    pygame.mixer.music.play(loops = 2, start = 10,fade_ms = 2)
-
+        # Plays the song called Striker
+        pygame.mixer.music.load("4-07. Striker.mp3")
+    #Plays the music loaded previously
+    pygame.mixer.music.play(loops = 2, start = 10,fade_ms =2)
 
 def setVolume(volume):
+    """
+    Sets the volume according to the number inputed
+
+    int -> None
+    """
     pygame.mixer.music.set_volume(volume)
 
-
 def fadeMusicOut(fadeAmount):
+    """
+    Fades out the music currently playing. The number inputed represents the delay in seconds the fade out should take
+
+    int -> None
+    """
     pygame.mixer.music.fadeout(fadeAmount)
 
-
 def stopMusic():
-    pygame.mixer.music.stop()
+    """
+    Stops the music currently playing
 
+    () -> None
+    """
+    pygame.mixer.music.stop()
 
 if __name__ == "__main__":
     initilizeMusic()
