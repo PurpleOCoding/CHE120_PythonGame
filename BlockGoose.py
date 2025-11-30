d980536a3d0ec07575d462e9c49d5361a3fc8568
INFECTER <steven.hedgy@gmail.com>
2025-11-30 10:21:48 -0500

    Commented BlockGoose.py

--- BlockGoose.py
+++ BlockGoose.py
@@ -9,15 +9,28 @@ import che120_questions
 
 from BaseBlock import BaseBlock
 class BlockGoose(BaseBlock):
-    
+    """
+    This block is a subclass of BaseBlock and represents the goose
+    """
     def __init__(self,coor,blockId,screen):
+        """
+        Constructor function contains a additional parameter from BlockBase which is the screen.
+        The function is used to initialize a object of this class type
+        """
         super().__init__(coor,blockId)
         self.screen = screen
         
         
         
     def beforeSteppingOnCell(self):
+        """
+        This starts a battle with the goose if the players health is not 100
+
+        () -> None
+        """
+        #Checks the players health to make sure it is not 100
         if(Damage.student_health() != 100):
+            #If player health is not 100 starts the fight with the goose
             Damage.heal_student(che120_questions.Goose(self.screen))
     
         
